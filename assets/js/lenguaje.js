99580477c695a1bcc5a6baafaed22acd80d76c1c
Tiagotin <tiagoguaglianoo@gmail.com>
2024-10-01 17:03:36 -0300

    ingles

--- assets/js/lenguaje.js
+++ assets/js/lenguaje.js
@@ -2,20 +2,34 @@ document.addEventListener('DOMContentLoaded', () => {
     const languageToggleButton = document.getElementById('language-toggle');
     const elements = document.querySelectorAll('[data-es]');
 
-    let currentLanguage = 'es'; // Idioma predeterminado
+    // Cargar el idioma guardado en localStorage o usar 'es' como predeterminado
+    let currentLanguage = localStorage.getItem('selectedLanguage') || 'es';
 
-    languageToggleButton.addEventListener('click', () => {
-        if (currentLanguage === 'es') {
-            currentLanguage = 'en';
-            languageToggleButton.textContent = 'Cambiar a Español';
-        } else {
-            currentLanguage = 'es';
-            languageToggleButton.textContent = 'Switch to English';
-        }
-
-        // Cambiar el contenido de cada elemento
+    // Función para actualizar el contenido según el idioma seleccionado
+    const updateContent = () => {
         elements.forEach(el => {
             el.textContent = el.getAttribute(`data-${currentLanguage}`);
         });
+
+        // Cambiar el texto del botón según el idioma actual
+        if (currentLanguage === 'es') {
+            languageToggleButton.textContent = 'Switch to English';
+        } else {
+            languageToggleButton.textContent = 'Cambiar a Español';
+        }
+    };
+
+    // Aplicar el idioma guardado al cargar la página
+    updateContent();
+
+    // Evento para cambiar el idioma al hacer clic en el botón
+    languageToggleButton.addEventListener('click', () => {
+        currentLanguage = currentLanguage === 'es' ? 'en' : 'es';
+
+        // Guardar el idioma seleccionado en localStorage
+        localStorage.setItem('selectedLanguage', currentLanguage);
+
+        // Actualizar el contenido después del cambio de idioma
+        updateContent();
     });
 });
